83c75f11960678384d642796285af8b7e351b42a
ShadowfeindX <the_specialist@programmer.net>
2017-05-22 22:22:25 -0400

    Update erl_discord.ex

--- lib/erl_discord.ex
+++ lib/erl_discord.ex
@@ -15,4 +15,15 @@ defmodule ED do
   def hello do
     :world
   end
+  
+  def handle_event({event, _payload}, state) do
+    IO.puts "Received Event: #{event}"
+    {:ok, state}
+  end
+
 end
+
+{:ok, bot_client } = DiscordEx.Client.start_link(%{
+	token: MzA2MjY0NjMzNjkyMzg5Mzg2.DAUuUw.7_U4Oqg7x5Mn90BfRzzrD02gnGE,
+	handler: ED
+})
