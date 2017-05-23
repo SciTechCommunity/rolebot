640927162dead6fd086b5b3c0940ecfae8ee22b8
ShadowfeindX <the_specialist@programmer.net>
2017-05-23 00:09:08 -0400

    Update erl_discord.ex

--- lib/erl_discord.ex
+++ lib/erl_discord.ex
@@ -16,9 +16,12 @@ defmodule ED do
     :world
   end
   
+  def state do: Process.get :state
+  
   def handle_event({:message_create, payload}, state) do
     IO.puts "Received Message Create Event"
     IO.inspect payload
+    Process.put :state, state
     {:ok, state}
   end
   
