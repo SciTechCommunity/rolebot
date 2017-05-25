66410b956b28ae8f93b282b8f85a3020124b3f3f
ShadowfeindX <the_specialist@programmer.net>
2017-05-25 03:57:13 -0400

    Update and rename start.exs to lib/main.ex

--- lib/main.ex
+++ lib/main.ex
@@ -1,6 +1,8 @@
+defmodule Main do
 token = "Enter your bot token: " |> IO.gets |> String.trim
 HTTPoison.start
 {:ok, bot_client } = DiscordEx.Client.start_link(%{
 	token: "Bot " <> token,
 	handler: ED
 })
+end
