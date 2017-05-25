8e12a7fa0c4a43a890ec3fee2fe26bf1ef321531
ShadowfeindX <the_specialist@programmer.net>
2017-05-24 23:35:25 -0400

    Update erl_discord.ex

--- lib/erl_discord.ex
+++ lib/erl_discord.ex
@@ -17,25 +17,26 @@ defmodule ED do
   end
   
   defp send_message(msg, ch, conn), do: DiscordEx.RestClient.Resources.Channel.send_message conn, ch, %{content: msg}
-  defp greeting, do: ["Hello!", "Hi!", "Hey!", "Howdy!", "Hiya!", "HeyHi!", "Greetings!"]
   
+  defp _greet, do: ["Hello!", "Hi!", "Hey!", "Howdy!", "Hiya!", "HeyHi!", "Greetings!"]
   def greet(conn, channel) do
-    greeting
+    _greet()
       |> Enum.random
-      |> send_message channel, conn
+      |> send_message(channel, conn)
       |> IO.inspect
   end
+  
+  def add_member_role(role, state, payload), do: IO.inspect {role, state, payload}
   
   def handle_event({:message_create, payload}, state) do
     IO.puts "Received Message Create Event"
     case payload |> DiscordEx.Client.Helpers.MessageHelper.msg_command_parse do
       {"hello", _} -> greet state[:rest_client], payload[:data]["channel_id"]
-      {"add", "role " <> role} -> role |> URI.encode_www_form |> IO.inspect
+      {"add", "role " <> role} -> add_member_role role, state[:rest_client], payload[:data]
       other -> other |> IO.inspect
     end
     {:ok, state}
-  end
-  
+  end 
   def handle_event({event, _payload}, state) do
     IO.puts "Received Event: #{event}"
     {:ok, state}
