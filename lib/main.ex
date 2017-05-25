359d686bf895d23e0f2e929e2365e552c1fb1431
ShadowfeindX <the_specialist@programmer.net>
2017-05-25 04:00:15 -0400

    Update main.ex

--- lib/main.ex
+++ lib/main.ex
@@ -1,8 +1,16 @@
 defmodule Main do
-token = "Enter your bot token: " |> IO.gets |> String.trim
-HTTPoison.start
-{:ok, bot_client } = DiscordEx.Client.start_link(%{
-	token: "Bot " <> token,
-	handler: ED
-})
+  def main(_args \\ []) do
+    token = "Enter your bot token: " |> IO.gets |> String.trim
+    HTTPoison.start
+    {:ok, bot_client } = DiscordEx.Client.start_link(%{
+      token: "Bot " <> token,
+      handler: ED
+    })
+    start
+  end
+  defp start do
+    receive do
+      e -> IO.inspect e
+    end
+    start
 end
