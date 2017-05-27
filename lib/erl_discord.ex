d5f04c037cefabf0f731ab664190293f2002e6ee
ShadowfeindX <the_specialist@programmer.net>
2017-05-27 04:42:14 -0400

    Update erl_discord.ex

--- lib/erl_discord.ex
+++ lib/erl_discord.ex
@@ -12,11 +12,11 @@ defmodule ED do
   defp channels(ch), do: Access.get %{317915118060961793 => :welcome}, ch
   defp roles(r), do: Access.get %{visitor: 292739861767782401, member: 235927353832767498}, r
   defp welcome(payload, state) do
-    case channels(payload["channel_id"]) do
+    case payload["channel_id"] |> channels do
       :welcome ->
         [ guild | _ ] = state[:guilds]
         add_member_role state[:rest_client], guild[:guild_id], payload["author"]["id"], roles(:visitor)
-        delete_message payload["id"], channels[:welcome], state[:rest_client]
+        delete_message payload["id"], payload["channel_id"], state[:rest_client]
       nil -> {:unknown, payload["channel_id"]}
     end
   end
